d2fa59ee85316ea6c5585dc0a6e7c5813746afd3
Coldsky22 <bingalik@mail.ru>
2023-06-24 03:30:29 +0300

    rev02

--- backend/api/utils.py
+++ backend/api/utils.py
@@ -10,9 +10,12 @@ def file_creation(shoping_list):
     with open("example.txt", "w+") as file_name:
         for line_list in shoping_list:
             ingredient = Ingredient.objects.get(pk=line_list['ingredient'])
-            file_name.write(f'{ingredient.name} 'f'({ingredient.measurement_unit})'f' - {line_list["count"]}\n')
+            file_name.write(
+                f'{ingredient.name} 'f'({ingredient.measurement_unit})'
+                f' - {line_list["count"]}\n')
     with open("example.txt", "r") as read_file:
-        response = HttpResponse(read_file.read(),content_type="text/plain,charset=utf8")
+        response = HttpResponse(
+            read_file.read(), content_type="text/plain,charset=utf8")
         message = 'attachment; filename="{}.txt"'.format('file_name')
         response['Content-Disposition'] = message
         remove("example.txt")
--- backend/app/management/commands/imports_csv.py
+++ backend/app/management/commands/imports_csv.py
@@ -4,20 +4,21 @@ import logging
 from app.models import Ingredient
 from django.core.management import BaseCommand
 
+
 class Command(BaseCommand):
-
-
     def add_arguments(self, parser):
-        parser.add_argument('csv_file', nargs='+', type=str)
-        parser.add_argument('--log_file', type=str, help='path to the log file')
+        parser.add_argument("csv_file", nargs="+", type=str)
+        parser.add_argument("--log_file", type=str,
+                            help="path to the log file")
 
     def handle(self, *args, **options):
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        formatter = logging.Formatter(
+            "%(asctime)s - %(levelname)s - %(message)s")
 
-        if options.get('log_file'):
-            file_handler = logging.FileHandler(options['log_file'])
+        if options.get("log_file"):
+            file_handler = logging.FileHandler(options["log_file"])
             file_handler.setLevel(logging.INFO)
             file_handler.setFormatter(formatter)
             logger.addHandler(file_handler)
@@ -26,14 +27,13 @@ class Command(BaseCommand):
         console_handler.setFormatter(formatter)
         logger.addHandler(console_handler)
 
-        for csv_file in options['csv_file']:
-            data_reader = csv.reader(open(csv_file), delimiter=',')
+        for csv_file in options["csv_file"]:
+            data_reader = csv.reader(open(csv_file), delimiter=",")
             for row in data_reader:
                 try:
                     Ingredient.objects.update_or_create(
-                        name=row[0],
-                        measurement_unit=row[1]
+                        name=row[0], measurement_unit=row[1]
                     )
-                    logger.info('Ingredient {} added'.format(row[0]))
+                    logger.info("Ingredient {} added".format(row[0]))
                 except Exception as e:
                     logger.error(str(e))
